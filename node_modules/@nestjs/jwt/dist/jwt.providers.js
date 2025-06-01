859f2687c06e40eec072482e37bff3d8da8b08a6
Adi Primanda Ginting <adi@adipginting.com>
2025-06-01 18:42:22 +0700

    fix: revert mistaken installed node_modules

--- node_modules/@nestjs/jwt/dist/jwt.providers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createJwtProvider = createJwtProvider;
-const jwt_constants_1 = require("./jwt.constants");
-function createJwtProvider(options) {
-    return [{ provide: jwt_constants_1.JWT_MODULE_OPTIONS, useValue: options || {} }];
-}
